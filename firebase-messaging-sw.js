760483e32e7a2a16e4cf887d2ab3b284bb18865e
pxerapxn <peerapon190345@gmail.com>
2026-09-17 13:33:07 +0700

    Refactor Firebase messaging service worker setup

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,46 +1,183 @@
+/* =====================================================
+   FIREBASE MESSAGING SERVICE WORKER
+===================================================== */
+
+
 importScripts(
   "https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js"
 );
+
 
 importScripts(
   "https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js"
 );
 
+
+/* =====================================================
+   FIREBASE CONFIG
+===================================================== */
+
 firebase.initializeApp({
-  apiKey: "AIzaSyC2d9YhQYkDJpPz-kK9MFgoeN9JNhAOM",
-  authDomain: "ola-job-f75b0.firebaseapp.com",
-  projectId: "ola-job-f75b0",
-  storageBucket: "ola-job-f75b0.firebasestorage.app",
-  messagingSenderId: "619901142506",
-  appId: "1:619901142506:web:bf1aec74f6448bf6ffde93",
-  measurementId: "G-4DC0PHEQH1"
+
+  apiKey:
+    "AIzaSyC2d9YhQYkJDJpPz-kK9MFgoeN9JNhAOM",
+
+  authDomain:
+    "ola-job-f75b0.firebaseapp.com",
+
+  projectId:
+    "ola-job-f75b0",
+
+  storageBucket:
+    "ola-job-f75b0.firebasestorage.app",
+
+  messagingSenderId:
+    "619901142506",
+
+  appId:
+    "1:619901142506:web:bf1aec74f6448bf6ffde93",
+
+  measurementId:
+    "G-4DC0PHEQH1"
+
 });
 
-const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
+const messaging =
+  firebase.messaging();
 
-  console.log(
-    "[firebase-messaging-sw.js] Background message:",
-    payload
-  );
 
-  const notificationTitle =
-    payload.notification?.title || "🔔 JOB TIME";
+/* =====================================================
+   BACKGROUND MESSAGE
+===================================================== */
 
-  const notificationOptions = {
-    body:
-      payload.notification?.body ||
-      "ถึงเวลาปฏิบัติงานแล้ว",
-    icon: "./icon-192.png",
-    badge: "./icon-192.png",
-    tag: "job-time",
-    requireInteraction: true,
-    vibrate: [300, 200, 300]
-  };
+messaging.onBackgroundMessage(
+  function(payload){
 
-  self.registration.showNotification(
-    notificationTitle,
-    notificationOptions
-  );
-});
+    console.log(
+      "[firebase-messaging-sw.js] Background message:",
+      payload
+    );
+
+
+    const notificationTitle =
+      payload.notification?.title ||
+      payload.data?.title ||
+      "🔔 JOB TIME";
+
+
+    const notificationOptions = {
+
+      body:
+        payload.notification?.body ||
+        payload.data?.body ||
+        "ถึงเวลาปฏิบัติงานแล้ว",
+
+      icon:
+        "./icon-192.png",
+
+      badge:
+        "./icon-192.png",
+
+      tag:
+        payload.data?.tag ||
+        "job-time",
+
+      requireInteraction:
+        true,
+
+      vibrate:
+        [
+          300,
+          200,
+          300
+        ],
+
+      data: {
+
+        url:
+          payload.data?.url ||
+          "./"
+
+      }
+
+    };
+
+
+    return self.registration.showNotification(
+
+      notificationTitle,
+
+      notificationOptions
+
+    );
+
+  }
+);
+
+
+/* =====================================================
+   NOTIFICATION CLICK
+===================================================== */
+
+self.addEventListener(
+  "notificationclick",
+  event => {
+
+    event.notification.close();
+
+
+    const url =
+      event.notification?.data?.url ||
+      "./";
+
+
+    event.waitUntil(
+
+      clients
+        .matchAll({
+
+          type:
+            "window",
+
+          includeUncontrolled:
+            true
+
+        })
+
+        .then(
+          clientList => {
+
+            for(
+              const client
+              of clientList
+            ){
+
+              if(
+                "focus" in client
+              ){
+
+                return client.focus();
+
+              }
+
+            }
+
+
+            if(
+              clients.openWindow
+            ){
+
+              return clients.openWindow(
+                url
+              );
+
+            }
+
+          }
+        )
+
+    );
+
+  }
+);
